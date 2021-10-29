ea9eae402b6b5d1e3ddd85250f0a522379138a06
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-10-29 16:51:38 -0400

    Added to entry file, changed start script, changed query to options

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,9 +1,11 @@
-    import React from "react";
-    import ReactDOM from "react-dom";
-    
-    document.addEventListener("DOMContentLoaded", () => {
-        const store = configureStore();
+import React from "react";
+import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root";
 
-        const root = document.getElementById("root");
-        ReactDOM.render(<Root store={store} />, root);
-    })
+document.addEventListener("DOMContentLoaded", () => {
+    const store = configureStore();
+
+    const root = document.getElementById("root");
+    ReactDOM.render(<Root store={store} />, root);
+})
