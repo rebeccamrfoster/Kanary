e3c790bf65da926f636e8c79fb5303878db57f69
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-10-29 12:11:16 -0400

    Migrated genres table and created validations in genre model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_29_155857) do
+ActiveRecord::Schema.define(version: 2021_10_29_160623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_genres_on_name", unique: true
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title", null: false
