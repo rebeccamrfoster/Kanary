d568ada57e46d5e32f2135bfd2e95fa348792e37
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-10-29 21:17:35 -0400

    Changed login and signup routes to auth routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,14 +3,15 @@ import { Route } from "react-router";
 import Navbar from "./navbar";
 import SignupFormContainer from "./session_form/signup_form_container";
 import LoginFormContainer from "./session_form/login_form_container";
+import { AuthRoute, ProtectedRoute } from "../utils/route_util";
 
 const App = () => {
     return (
         <div>
             <Navbar />
 
-            <Route path="/signup" component={SignupFormContainer} />
-            <Route path="/login" component={LoginFormContainer} />
+            <AuthRoute path="/signup" component={SignupFormContainer} />
+            <AuthRoute path="/login" component={LoginFormContainer} />
         </div>
 
     )
