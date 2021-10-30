77c30545837f4efc6af291a731bc131becfdcad8
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-10-29 22:52:08 -0400

    Added routes for movies

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
+    resources :movies, only: [:index, :show]
     
     resource :session, only: [:create, :destroy]
   end
