bd10524a6225e9950fc65766c24d9c57bf07b118
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-10-30 12:29:37 -0400

    Added navbar container and protected route for movie index

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { Route } from "react-router";
-import Navbar from "./navbar";
+import NavbarContainer from "./navbar/navbar";
 import SignupFormContainer from "./session_form/signup_form_container";
 import LoginFormContainer from "./session_form/login_form_container";
+import MovieIndexContainer from "./movie_index/movie_index_container";
 import { AuthRoute, ProtectedRoute } from "../utils/route_util";
+import { Switch } from "react-router";
 
 const App = () => {
     return (
         <div>
-            <Navbar />
+            <NavbarContainer />
 
-            <AuthRoute path="/signup" component={SignupFormContainer} />
-            <AuthRoute path="/login" component={LoginFormContainer} />
+            <Switch>
+                <AuthRoute exact path="/signup" component={SignupFormContainer} />
+                <AuthRoute exact path="/login" component={LoginFormContainer} />
+                <ProtectedRoute path="/movies" component={MovieIndexContainer} />
+            </Switch>
         </div>
 
     )
