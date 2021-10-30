066caaeed3e1e31d5daee1eebd116bde83780839
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-10-29 20:35:23 -0400

    Added routes for signup and login

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Route } from "react-router";
+import Navbar from "./navbar";
+import SignupFormContainer from "./session_form/signup_form_container";
+import LoginFormContainer from "./session_form/login_form_container";
 
 const App = () => {
     return (
-        <h1>Kanary</h1>
+        <div>
+            <Navbar />
+
+            <Route path="/signup" component={SignupFormContainer} />
+            <Route path="/login" component={LoginFormContainer} />
+        </div>
+
     )
 };
 
