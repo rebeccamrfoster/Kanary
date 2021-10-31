19812a39dc2ba71edfc57ca071fe8931ee2d04e8
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-10-30 23:31:19 -0400

    Added index and show routes for genres

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
     resources :movies, only: [:index, :show]
-    
+    resources :genres, only: [:index, :show]
+
     resource :session, only: [:create, :destroy]
   end
 
