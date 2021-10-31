45ea11764bea0f9c2122f4e783ba066b5707171c
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-10-30 23:30:49 -0400

    Changed render show to file path and added includes for genres

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,11 +1,11 @@
 class Api::MoviesController < ApplicationController
     def index
-        @movies = Movie.all
+        @movies = Movie.all.includes(:genres)
         render "/api/movies/index"
     end
 
     def show
         @movie = Movie.find(params[:id])
-        render :show
+        render "/api/movies/show"
     end
 end
