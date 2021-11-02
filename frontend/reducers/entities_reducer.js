de6e07180010680334b0060aff54bd8d72f14252
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-01 23:14:36 -0400

    Added watchlists reducer to entities

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import usersReducer from "./users_reducer";
 import moviesReducer from "./movies_reducer";
 import genresReducer from "./genres_reducer";
+import watchlistsReducer from "./watchlists_reducer";
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     movies: moviesReducer,
-    genres: genresReducer
+    genres: genresReducer,
+    watchlists: watchlistsReducer
 });
 
 export default entitiesReducer;
