8352b30419778f08a31d1afeeb4336ab26fe231e
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-03 14:24:58 -0400

    Completed first draft of styling for splash component

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,21 +4,21 @@ import { Link } from "react-router-dom";
 const Splash = () => {
     return (
         <div className="splash">
+            <Link to="/" className="logo">kanary</Link>
+
             <Link className="login" to="/login">
                 <h1>LOG IN</h1>
             </Link>
+            
             <div className="content">
-                <div className="content__container">
-                    <h1 className="content__logo">Kanary</h1>
-                    <h1 className="content__header">Enjoy Thoughtful Entertainment</h1>
-                    <p className="content__body">Stream thousands of films for free, thanks to the generous support of your public library or university</p>
-                </div>
+                <h1 className="header-content">Enjoy Thoughtful Entertainment</h1>
+                <p className="body-content">Stream thousands of films for free, thanks to the generous support of your public library or university</p>
+                <Link to="/signup" className="signup-link-content">
+                    <h1>GET STARTED</h1>
+                </Link>
             </div>
+
             <img src={window.splashUrl} />
-            {/* <img src="https://static.kanopy.com/images/webshop-landing/hero-toppers/US/pl/1d-landing-page-hero-us-pl-1-moonlight.jpg" /> */}
-            {/* <div id="bg">
-                <img className="hero" src="https://kanary-bucket.s3.us-east-2.amazonaws.com/splash_1.jpg" />
-            </div> */}
         </div>
     )
 };
