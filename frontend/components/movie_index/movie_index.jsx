32dc02d680666eee136077e87c9073e1976bfcae
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-03 10:05:58 -0400

    Changed deconstructed props and props passed to child

--- frontend/components/movie_index/movie_index.jsx
+++ frontend/components/movie_index/movie_index.jsx
@@ -9,12 +9,15 @@ class MovieIndex extends React.Component {
     componentDidMount() {
         this.props.fetchMovies();
         this.props.fetchGenres();
+        this.props.fetchWatchlists();
     }
 
     render() {
-        const { genres, movies } = this.props;
+        const { genres, movies, watchlists, createWatchlist, deleteWatchlist, currentUser } = this.props;
         
-        if (genres.length === 0 || movies.length === 0) return null;
+        if (genres.length === 0 || 
+            movies.length === 0 || 
+            watchlists.length === 0) return null;
         return (
             <div>
                 <h1>All Movies</h1>
@@ -22,7 +25,11 @@ class MovieIndex extends React.Component {
                     genres.map(genre => (
                         <GenreCarousel key={genre.id}
                                        genre={genre}
-                                       allMovies={movies} />
+                                       allMovies={movies}
+                                       watchlists={watchlists}
+                                       createWatchlist={createWatchlist}
+                                       deleteWatchlist={deleteWatchlist}
+                                       currentUser={currentUser} />
                     ))
                 }
             </div>
