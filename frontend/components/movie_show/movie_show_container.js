665ba2b7fe4dc184d63c66680e109fef690509a7
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-04 23:16:16 -0400

    Changed map dispatch to props to contain handle watchlist action

--- frontend/components/movie_show/movie_show_container.js
+++ frontend/components/movie_show/movie_show_container.js
@@ -1,6 +1,7 @@
 import { connect } from "react-redux";
 import MovieShow from "./movie_show";
 import { fetchMovie } from "../../actions/movie_actions";
+import { fetchWatchlists, handleWatchlist } from "../../actions/watchlist_actions";
 
 const mSTP = (state, ownProps) => {
     return {
@@ -13,8 +14,7 @@ const mDTP = dispatch => {
     return {
         fetchMovie: movieId => dispatch(fetchMovie(movieId)),
         fetchWatchlists: () => dispatch(fetchWatchlists()),
-        createWatchlist: watchlist => dispatch(createWatchlist(watchlist)),
-        deleteWatchlist: watchlistId => dispatch(deleteWatchlist(watchlistId))
+        handleWatchlist: (user, movie) => dispatch(handleWatchlist(user, movie))
     }
 };
 
