fe9618c23ff247bd87eebda8f66e84ba09d8a15f
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-04 21:17:53 -0400

    Added protected route for movie show page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,7 @@ import NavbarContainer from "./navbar/navbar_container";
 import SignupFormContainer from "./session_form/signup_form_container";
 import LoginFormContainer from "./session_form/login_form_container";
 import MovieIndexContainer from "./movie_index/movie_index_container";
+import MovieShowContainer from "./movie_show/movie_show_container";
 import { AuthRoute, ProtectedRoute } from "../utils/route_util";
 import { Route, Switch } from "react-router";
 import Splash from "./splash/splash";
@@ -18,6 +19,7 @@ const App = () => {
             <Switch>
                 <AuthRoute exact path="/signup" component={SignupFormContainer} />
                 <AuthRoute exact path="/login" component={LoginFormContainer} />
+                <ProtectedRoute path="/movies/:movieId" component={MovieShowContainer} />
                 <ProtectedRoute path="/movies" component={MovieIndexContainer} />
             </Switch>
         </div>
