7c0c0fe2ccfc48a6d3c292b0320be95212d88427
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-04 20:51:13 -0400

    Broke text onto multiple lines for readability

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -9,8 +9,13 @@ const Splash = () => {
             <Link className="login" to="/login">LOG IN</Link>
             
             <div className="content">
-                <h1 className="header-content">Enjoy Thoughtful Entertainment</h1>
-                <p className="body-content">Stream thousands of films for free, thanks to the generous support of your public library or university</p>
+                <h1 className="header-content">
+                    Enjoy Thoughtful Entertainment
+                </h1>
+                <p className="body-content">
+                    Stream thousands of films for free, thanks to the generous 
+                    support of your public library or university
+                </p>
                 <Link to="/signup" className="signup-link-content">
                     <h1>GET STARTED</h1>
                 </Link>
