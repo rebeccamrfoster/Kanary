fb55c4d8bdab6489d502a59e451fc25f651fca6b
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-06 08:23:53 -0400

    Added currentUser, genres, and fetchGenres to movie show container

--- frontend/components/movie_show/movie_show.jsx
+++ frontend/components/movie_show/movie_show.jsx
@@ -1,35 +1,109 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
+import Popup from "./popup";
 
 class MovieShow extends React.Component {
     constructor(props) {
         super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+        this.toggleIcon = this.toggleIcon.bind(this);
     }
 
     componentDidMount() {
-        this.props.fetchMovie(this.props.match.params.movieId)
+        this.props.fetchGenres();
         this.props.fetchWatchlists();
+        this.props.fetchMovie(this.props.match.params.movieId)
+            .then(() => {
+                const { currentUser, movie } = this.props;
+                const icon = currentUser.movieIds.includes(movie.id) ? (
+                    window.check_icon
+                ) : (
+                    window.plus_icon
+                );
+                this.setState({ icon });
+            })
+    }
+
+    handleClick() {
+        const { currentUser, movie, handleWatchlist } = this.props;
+        handleWatchlist(currentUser, movie)
+            .then(this.toggleIcon);
+    }
+
+    toggleIcon() {
+        // const root = document.getElementById("root");
+        // const div = document.createElement("div");
+        // div.id = "popup";
+        // root.appendChild(div);
+
+        if (this.state.icon === window.plus_icon) {
+            this.setState({ icon: window.check_icon });
+        }
+        else {
+            this.setState({ icon: window.plus_icon });
+        }
     }
 
     render() {
-        if (!this.props.movie) return null;
+        if (!this.props.movie || !this.props.genres || !this.state) return null;
 
-        const { title, year, duration, description, director } = this.props.movie;
+        // const {
+        //     movie: { title, year, duration, description, director, genreIds },
+        //     genres
+        // } = this.props;
+
+        const { title, year, duration, description, director, genreIds } = this.props.movie;
+        const { genres } = this.props;
 
         return (
             <div className="movie-show">
                 <div className="main-movie-show">
                     <video></video>
                     <h1>{title}</h1>
-                    <button>
-                        <img src={window.white_check_icon} />
+                    <button onClick={this.handleClick}>
+                        <img src={this.state.icon} />
                         <h1>My List</h1>
                     </button>
                 </div>
 
                 <div className="info-movie-show">
-
+                    <p className="description">{description}</p>
+                    <div className="container">
+                        <p>Running Time</p><p>{duration} mins</p>
+                        <p>Year</p><p>{year}</p>
+                        <p>Filmmaker</p><p>{director}</p>
+                        <p>Subjects</p>
+                        <div>
+                            {
+                                genreIds.map(genreId => {
+                                    return (
+                                        <div key={genreId} className="genres">
+                                            <Link to="/">
+                                                {`> ${genres[genreId].name}`}
+                                            </Link>
+                                            <br />
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
                 </div>
 
+                {
+                    this.state.icon === window.plus_icon ? (
+                        <Popup key={this.props.movie.id}
+                            added={true}
+                            title={this.props.movie.title} />
+                    ) : (
+                        <Popup key={this.props.movie.id}
+                            added={false}
+                            title={this.props.movie.title} />
+                    )
+                }
+                
 
             </div>
         )
--- frontend/components/movie_show/movie_show_container.js
+++ frontend/components/movie_show/movie_show_container.js
@@ -1,18 +1,21 @@
 import { connect } from "react-redux";
 import MovieShow from "./movie_show";
 import { fetchMovie } from "../../actions/movie_actions";
+import { fetchGenres } from "../../actions/genre_actions";
 import { fetchWatchlists, handleWatchlist } from "../../actions/watchlist_actions";
 
 const mSTP = (state, ownProps) => {
     return {
+        currentUser: state.entities.users[state.session.currentUserId],
         movie: state.entities.movies[ownProps.match.params.movieId],
-        watchlists: Object.values(state.entities.watchlists)
+        genres: state.entities.genres
     }
 };
 
 const mDTP = dispatch => {
     return {
         fetchMovie: movieId => dispatch(fetchMovie(movieId)),
+        fetchGenres: () => dispatch(fetchGenres()),
         fetchWatchlists: () => dispatch(fetchWatchlists()),
         handleWatchlist: (user, movie) => dispatch(handleWatchlist(user, movie))
     }
