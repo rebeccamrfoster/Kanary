212c521ad1eb7ca2c019fedfa650e60efbbcca3c
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-06 08:26:14 -0400

    Created popup components and completed minimal styling

---
+++ frontend/components/movie_show/popup.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { ReactDOM } from "react-dom";
+
+class Popup extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    clearPopup() {
+        // $("#popup").fadeOut(3000);
+
+        // const div = document.getElementById("popup");
+        // document.querySelector(".popup").classList.add("hide");
+        // div.parentNode.removeChild(div);
+    }
+
+    // componentDidMount() {
+        // const div = document.getElementById("popup");
+        // document.querySelector(".popup").classList.add("show");
+        // setTimeout(this.clearPopup, 3000);
+    // }
+
+    componentDidMount() {
+        let el = document.querySelector('.popup');
+        el.classList.add('fade-in');
+    }
+
+    render() {
+        const { added, title } = this.props;
+
+        return(
+            added ? (
+                <div className="popup">
+                    <div className="header-popup">
+                        <h1>Video added</h1>
+                        <img src={window.close_icon} onClick={this.clearPopup} />
+                    </div>
+                    <p className="body-popup">
+                        <i>{title}</i> has been added to your watchlist
+                    </p>
+                </div>
+            ) : (
+                <div className="popup">
+                    <div className="header-popup">
+                        <h1>Video removed</h1>
+                        <img src={window.close_icon} onClick={this.clearPopup} />
+                    </div>
+                    <p className="body-popup">
+                        <i>{title}</i> has been removed from your watchlist
+                    </p>
+                </div>
+            )
+        )
+    }
+}
+
+export default Popup;
