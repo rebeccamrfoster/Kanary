8a4fcac13a431daef8a916fd158e160542d9778f
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-08 17:12:10 -0500

    Returned empty object in remove watchlist action

--- frontend/reducers/watchlists_reducer.js
+++ frontend/reducers/watchlists_reducer.js
@@ -17,11 +17,12 @@ const watchlistsReducer = (oldState = {}, action) => {
             return nextState;
         case REMOVE_WATCHLIST:
             const watchlists = Object.values(nextState);
+            if (watchlists.length === 0) return {};
+
             const toRemove = watchlists.find(watchlist => (
                 watchlist.user_id === action.watchlist.user_id &&
                 watchlist.movie_id === action.watchlist.movie_id
             ));
-
             delete nextState[toRemove.id];
             return nextState;
         default:
