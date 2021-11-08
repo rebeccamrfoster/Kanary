d89130b2ca4c8d3c7fb28f64a96f2ab6dfbb9d04
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-07 21:51:14 -0500

    Created footer component

---
+++ frontend/components/footer/footer.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Footer = props => {
+
+    return (
+        <footer className="footer">
+            <section className="footer-gradient"></section>
+
+            <div className="upper-footer">
+                <div className="footer-portrait">
+                    <img src={window.portrait} />
+                </div>
+                <p>Rebecca Foster</p>
+                <p>Technologies</p>
+
+                <div className="footer-icons">
+                    <a className="footer-icon-link"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://github.com/rebeccamrfoster">
+                        <img src={window.github_icon} />
+                    </a>
+                    <a className="footer-icon-link"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.linkedin.com/in/rebecca-foster-791448203/">
+                        <img src={window.linkedin_icon} />
+                    </a>
+                </div>
+            </div>
+            
+            <p className="lower-footer">&#9400; 2021 Kanary</p>
+        </footer>
+    )
+};
+
+export default Footer;
