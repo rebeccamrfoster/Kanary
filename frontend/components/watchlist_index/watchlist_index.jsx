2e5f4e52ecb3515a7149d34724bccc6909d92ab2
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-08 13:52:58 -0500

    Created watchlist index and index items

--- frontend/components/watchlist_index/watchlist_index.jsx
+++ frontend/components/watchlist_index/watchlist_index.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import WatchlistIndexItem from "./watchlist_index_item";
 
-const WatchlistIndex = props => {
+class WatchlistIndex extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-    return (
-        <div>watchlist index</div>
-    )
-};
+    componentDidMount() {
+        this.props.fetchMovies();
+    }
+
+    render() {
+        if (Object.keys(this.props.movies).length === 0) return null;
+
+        const { currentUser, movies, handleWatchlist } = this.props;
+
+        return (
+            <div className="watchlist-index">
+                <h1 className="watchlist-index-title">My Watchlist</h1>
+                <div className="watchlist-index-main">
+                    {
+                        currentUser.movieIds.map(movieId => (
+                            <WatchlistIndexItem currentUser={currentUser}
+                                movie={movies[movieId]}
+                                handleWatchlist={handleWatchlist} />
+                        ))
+                    }
+                </div>
+            </div>
+        )
+    }
+}
 
 export default WatchlistIndex;
--- frontend/components/watchlist_index/watchlist_index_container.js
+++ frontend/components/watchlist_index/watchlist_index_container.js
@@ -1,16 +1,20 @@
 import { connect } from "react-redux";
+import { fetchWatchlists } from "../../actions/watchlist_actions";
 import WatchlistIndex from "./watchlist_index";
+import { fetchMovies } from "../../actions/movie_actions";
+import { handleWatchlist } from "../../actions/watchlist_actions";
 
 const mSTP = state => {
     return {
         currentUser: state.entities.users[state.session.currentUserId],
-        watchlists: Object.values(state.entities.watchlists)
+        movies: state.entities.movies
     }
 };
 
 const mDTP = dispatch => {
     return {
-
+        fetchMovies: () => dispatch(fetchMovies()),
+        handleWatchlist: (user, movie) => dispatch(handleWatchlist(user, movie))
     }
 };
 
