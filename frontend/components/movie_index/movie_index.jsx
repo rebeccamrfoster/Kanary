03a0e3253326f98f488ac2dc77075fc1a0799d0f
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-07 22:37:57 -0500

    Wrote text on top of banner image

--- frontend/components/movie_index/movie_index.jsx
+++ frontend/components/movie_index/movie_index.jsx
@@ -25,6 +25,8 @@ class MovieIndex extends React.Component {
         return (
             <div className="movie-index">
                 <div className="movie-index-banner">
+                    <p>Enjoy critically-acclaimed movies, inspiring 
+                        documentaries, award-winning foreign films and more</p>
                     <img src={window.movie_index_banner}/>
                 </div>
                 {
