72391a01dcc8a319f8738eaab480c6dd32433a74
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-07 22:11:10 -0500

    Made footer a protected route

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -24,7 +24,7 @@ const App = () => {
                 <ProtectedRoute path="/movies" component={MovieIndexContainer} />
             </Switch>
 
-            <Footer />
+            <ProtectedRoute component={Footer} />
         </div>
 
     )
