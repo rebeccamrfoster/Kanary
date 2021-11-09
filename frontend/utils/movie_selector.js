5fb2659bfc39a86251ce66f9debb2dc9cdc9af13
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-09 15:05:28 -0500

    Modified movie selector to take in movies and genres objects

--- frontend/utils/movie_selector.js
+++ frontend/utils/movie_selector.js
@@ -1,23 +1,23 @@
-export const selectMoviesBySearch = (state, query) => {
-    Object.freeze(state);
-    const movies = Object.values(state.entities.movies);
+export const selectMoviesBySearch = (movies, genres, query) => {
+    // Object.freeze(state);
+    // const movies = Object.values(state.entities.movies);
     query = query.toLowerCase();
 
-    return movies.filter(movie => {
+    return Object.values(movies).filter(movie => {
         return (
             movie.title.toLowerCase().includes(query) ||
             movie.description.toLowerCase().includes(query) ||
             movie.director.toLowerCase().includes(query) ||
-            selectMoviesByGenre(state, movie.id)
+            selectGenresByMovie(genres, movie)
                 .some(genre => genre.toLowerCase().includes(query))
         )
     });
 };
 
-const selectMoviesByGenre = (state, movieId) => {
-    Object.freeze(state);
-    const movie = state.entities.movies[movieId];
-    const genres = state.entities.genres;
+const selectGenresByMovie = (genres, movie) => {
+    // Object.freeze(state);
+    // const movie = state.entities.movies[movieId];
+    // const genres = state.entities.genres;
 
     return movie.genreIds.map(genreId => genres[genreId].name)
 };
