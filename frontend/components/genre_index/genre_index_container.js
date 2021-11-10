5209c65431b8b3d218af9153b3919c73642d15e1
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-10 15:01:26 -0500

    Added movies and currentUser to genre index container props

--- frontend/components/genre_index/genre_index_container.js
+++ frontend/components/genre_index/genre_index_container.js
@@ -1,20 +1,24 @@
 import { connect } from "react-redux";
+import { withRouter } from "react-router";
 import { fetchGenre } from "../../actions/genre_actions";
 import { fetchMovies } from "../../actions/movie_actions";
+import { handleWatchlist } from "../../actions/watchlist_actions";
 import GenreIndex from "./genre_index";
 
 const mSTP = (state, ownProps) => {
     return {
         genre: state.entities.genres[ownProps.match.params.genreId],
-        movies: state.entities.movies
+        movies: state.entities.movies,
+        currentUser: state.entities.users[state.session.currentUserId]
     }
 };
 
 const mDTP = dispatch => {
     return {
         fetchGenre: genreId => dispatch(fetchGenre(genreId)),
-        fetchMovies: () => dispatch(fetchMovies())
+        fetchMovies: () => dispatch(fetchMovies()),
+        handleWatchlist: (user, movie) => dispatch(handleWatchlist(user, movie))
     }
 };
 
-export default connect(mSTP, mDTP)(GenreIndex);
+export default withRouter(connect(mSTP, mDTP)(GenreIndex));
