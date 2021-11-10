c6beb13bef0c92253e29516c4f5a2bd698733329
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-10 15:00:55 -0500

    Wrote genre index component

--- frontend/components/genre_index/genre_index.jsx
+++ frontend/components/genre_index/genre_index.jsx
@@ -1,8 +1,7 @@
 import React from "react";
+import GenreCarouselItem from "../movie_index/genre_carousel_item";
 
 class GenreIndex extends React.Component {
-    
-
     componentDidMount() {
         this.props.fetchGenre(this.props.match.params.genreId);
         this.props.fetchMovies();
@@ -12,9 +11,24 @@ class GenreIndex extends React.Component {
         if (!this.props.genre || 
             Object.values(this.props.movies).length === 0) return null;
 
-        return (
-            <div className="genre-index">
+        const { genre, movies, currentUser, handleWatchlist, history } = this.props;
 
+        return (
+            <div className="genre-index movie-index">
+                <h1>{genre.name}</h1>
+                <div className="genre-index-main carousel">
+                    <div className="genre-index-movies carousel-main">
+                        {
+                            genre.movieIds.map(movieId => (
+                                <GenreCarouselItem key={movieId}
+                                    currentUser={currentUser}
+                                    movie={movies[movieId]}
+                                    handleWatchlist={handleWatchlist}
+                                    history={history} />
+                            ))
+                        }
+                    </div>
+                </div>
             </div>
         )
     }
