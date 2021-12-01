df2a392c1fa7c39456e7b1466f8c0513a778733f
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-11-30 22:20:44 -0500

    Refactor Popup component to call clearPopup prop

--- frontend/components/movie_show/popup.jsx
+++ frontend/components/movie_show/popup.jsx
@@ -1,21 +1,28 @@
 import React, { useState, useEffect } from "react";
 
-const Popup = ({ added, title }) => {
-    const [fadeProp, setFadeProp] = useState({ fade: "" });
+const Popup = ({ added, title, clearPopup }) => {
+    // const [fadeProp, setFadeProp] = useState({ fade: "" });
 
-    useEffect(() => {
-        const delay = setTimeout(() => {
-            const el = document.querySelector(".popup");
-            if (el.id === "") {
-                el.id = "fade-in"
-            }
-        }, 200)
-    })
+    const el = document.querySelector(".popup");
+    if (el) clearPopup();
 
     const handleClearPopup = () => {
         const el = document.querySelector(".popup");
         el.id = "fade-out";
+        setTimeout(() => {
+            clearPopup();
+        }, 600)
     };
+
+    useEffect(() => {
+        const delay = setTimeout(() => {
+            const el = document.querySelector(".popup");
+            if (!el) return;
+            if (el.id === "") {
+                el.id = "fade-in"
+            }
+        }, 200);
+    })
 
     return (
         <div id="" className="popup" onClick={handleClearPopup}>
