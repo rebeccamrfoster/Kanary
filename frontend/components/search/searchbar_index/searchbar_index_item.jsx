94071d4a5f65465f83f106404be6daa9a8d78368
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-12-03 09:23:53 -0500

    Replace explicit with implicit return

--- frontend/components/search/searchbar_index/searchbar_index_item.jsx
+++ frontend/components/search/searchbar_index/searchbar_index_item.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const SearchbarIndexItem = ({ movie }) => {
-    
-    return (
-        <Link to={`/movies/${movie.id}`} className="search-index-item">
-            <div className="search-thumbnail">
-                <div className="search-thumbnail-inner">
-                    <img key={movie.id} src={movie.thumbnail} />
-                    {/* <img src="https://m.media-amazon.com/images/M/MV5BODhkZGE0NDQtZDc0Zi00YmQ4LWJiNmUtYTY1OGM1ODRmNGVkXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" /> */}
-                </div>
+const SearchbarIndexItem = ({ movie }) => (
+    <Link to={`/movies/${movie.id}`} className="search-index-item">
+        <div className="search-thumbnail">
+            <div className="search-thumbnail-inner">
+                <img key={movie.id} src={movie.thumbnail} />
+                {/* <img src="https://m.media-amazon.com/images/M/MV5BODhkZGE0NDQtZDc0Zi00YmQ4LWJiNmUtYTY1OGM1ODRmNGVkXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" /> */}
             </div>
+        </div>
 
-            <div className="search-info">
-                <h1>{movie.title}</h1>
-                <p>{movie.description}</p>
-            </div>
-        </Link>
-    )
-};
+        <div className="search-info">
+            <h1>{movie.title}</h1>
+            <p>{movie.description}</p>
+        </div>
+    </Link>
+);
 
 export default SearchbarIndexItem;
