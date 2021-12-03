a017d4ae2e3f937a168814f379bf5660e64e43ed
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-12-03 09:16:27 -0500

    Replace class with functional component

--- frontend/components/movie_index/movie_index.jsx
+++ frontend/components/movie_index/movie_index.jsx
@@ -1,38 +1,32 @@
 import React from "react";
 import GenreCarousel from "./genre_carousel";
 
-class MovieIndex extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+const MovieIndex = props => {
+    const { currentUser, genres, movies, handleWatchlist, history } = props;
 
-    render() {
-        const { currentUser, genres, movies, handleWatchlist, history} = this.props;
-        
-        const nullRender = <div className="null-render"></div>;
-        if (Object.values(movies).length === 0) return nullRender;
-        if (!genres) return nullRender;
-        
-        return (
-            <div className="movie-index">
-                <div className="movie-index-banner">
-                    <p className="banner-text">Enjoy critically-acclaimed movies, inspiring 
-                        documentaries, award-winning foreign films and more</p>
-                    <img src={window.movie_index_banner}/>
-                </div>
-                {
-                    genres.map(genre => (
-                        <GenreCarousel key={genre.id}
-                            currentUser={currentUser}
-                            genre={genre}
-                            movies={movies}
-                            handleWatchlist={handleWatchlist}
-                            history={history} />
-                    ))
-                }
+    const nullRender = <div className="null-render"></div>;
+    if (Object.values(movies).length === 0) return nullRender;
+    if (!genres) return nullRender;
+    
+    return (
+        <div className="movie-index">
+            <div className="movie-index-banner">
+                <p className="banner-text">Enjoy critically-acclaimed movies, 
+                    inspiring documentaries, award-winning foreign films and more</p>
+                <img src={window.movie_index_banner}/>
             </div>
-        )
-    }
+            {
+                genres.map(genre => (
+                    <GenreCarousel key={genre.id}
+                        currentUser={currentUser}
+                        genre={genre}
+                        movies={movies}
+                        handleWatchlist={handleWatchlist}
+                        history={history} />
+                ))
+            }
+        </div>
+    )
 }
 
 export default MovieIndex;
