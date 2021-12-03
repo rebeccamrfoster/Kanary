137837e3976416455044c2906844fb0e92c0df74
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-12-03 09:28:37 -0500

    Replace explicit with implicit return

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,15 +3,12 @@ import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import App from "./app";
 
-const Root = (props) => {
-    const { store } = props;
-    return (
-        <Provider store={store}>
-            <HashRouter>
-                <App />
-            </HashRouter>
-        </Provider>
-    )
-};
+const Root = ({ store }) => (
+    <Provider store={store}>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </Provider>
+);
 
 export default Root;
