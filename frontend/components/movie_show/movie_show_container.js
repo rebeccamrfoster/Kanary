596e2f0b2501ad5e4ba7f3343b8ba7319a633c7a
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-12-03 09:18:49 -0500

    Restructure functions to have ES6 implicit returns

--- frontend/components/movie_show/movie_show_container.js
+++ frontend/components/movie_show/movie_show_container.js
@@ -1,22 +1,16 @@
 import { connect } from "react-redux";
 import MovieShow from "./movie_show";
-import { fetchMovie } from "../../actions/movie_actions";
-import { fetchGenres } from "../../actions/genre_actions";
 import { fetchWatchlists, handleWatchlist } from "../../actions/watchlist_actions";
 
-const mSTP = (state, ownProps) => {
-    return {
+const mSTP = (state, ownProps) => ({
         currentUser: state.entities.users[state.session.currentUserId],
         movie: state.entities.movies[ownProps.match.params.movieId],
         genres: state.entities.genres
-    }
-};
+});
 
-const mDTP = dispatch => {
-    return {
+const mDTP = dispatch => ({
         fetchWatchlists: () => dispatch(fetchWatchlists()),
         handleWatchlist: (user, movie) => dispatch(handleWatchlist(user, movie))
-    }
-};
+});
 
 export default connect(mSTP, mDTP)(MovieShow);
