cf337dee9f726c8c14dc0c289278e58e32fd14e1
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-12-03 09:24:50 -0500

    Restructure functions to have ES6 implicit returns

--- frontend/components/session_form/login_form_container.js
+++ frontend/components/session_form/login_form_container.js
@@ -2,18 +2,14 @@ import { connect } from "react-redux";
 import SessionForm from "./session_form";
 import { login, removeErrors, LOG_IN } from "../../actions/session_actions";
 
-const mSTP = state => {
-    return {
-        errors: state.errors.session,
-        formType: LOG_IN
-    }
-};
+const mSTP = state => ({
+    errors: state.errors.session,
+    formType: LOG_IN
+});
 
-const mDTP = dispatch => {
-    return {
-        processForm: user => dispatch(login(user)),
-        removeErrors: () => dispatch(removeErrors())
-    }
-};
+const mDTP = dispatch => ({
+    processForm: user => dispatch(login(user)),
+    removeErrors: () => dispatch(removeErrors())
+});
 
 export default connect(mSTP, mDTP)(SessionForm);
--- frontend/components/session_form/signup_form_container.js
+++ frontend/components/session_form/signup_form_container.js
@@ -2,19 +2,15 @@ import { connect } from "react-redux";
 import SessionForm from "./session_form";
 import { signup, login, removeErrors, SIGN_UP } from "../../actions/session_actions";
 
-const mSTP = state => {
-    return {
-        errors: state.errors.session,
-        formType: SIGN_UP
-    }
-};
+const mSTP = state => ({
+    errors: state.errors.session,
+    formType: SIGN_UP
+});
 
-const mDTP = dispatch => {
-    return {
-        processForm: user => dispatch(signup(user)),
-        demoLogin: user => dispatch(login(user)),
-        removeErrors: () => dispatch(removeErrors())
-    }
-};
+const mDTP = dispatch => ({
+    processForm: user => dispatch(signup(user)),
+    demoLogin: user => dispatch(login(user)),
+    removeErrors: () => dispatch(removeErrors())
+});
 
 export default connect(mSTP, mDTP)(SessionForm);
